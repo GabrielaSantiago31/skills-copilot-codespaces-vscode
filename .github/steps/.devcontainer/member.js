a2e2006de69b0c979b71a5f497b2694cf30f6f16
Gabriela Santiago <gabriela.santiago@capgemini.com>
2024-01-30 12:05:36 -0300

    Copilot third commit

---
+++ .github/steps/.devcontainer/member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+  return [
+    {
+      name: 'Member',
+      value: 'member',
+      short: 'Member',
+    },
+    {
+      name: 'Admin',
+      value: 'admin',
+      short: 'Admin',
+    },
+    {
+      name: 'Owner',
+      value: 'owner',
+      short: 'Owner',
+    },
+  ]
+}
